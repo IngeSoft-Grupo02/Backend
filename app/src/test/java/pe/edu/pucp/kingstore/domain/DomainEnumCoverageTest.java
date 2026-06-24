b750a8c185c87ae7aed2cef07b84a9bbf41fc22b
Christian Miraya <christianmiraya01@hotmailcom>
2026-06-24 16:09:32 -0500

    actualizado de enums

--- app/src/test/java/pe/edu/pucp/kingstore/domain/DomainEnumCoverageTest.java
+++ app/src/test/java/pe/edu/pucp/kingstore/domain/DomainEnumCoverageTest.java
@@ -23,16 +23,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DomainEnumCoverageTest {
 
-//    @Test
-//    void exposesStorePaletteAndProductSizeEnums() {
-//        assertThat(Size.values()).containsExactly(Size.XS, Size.S, Size.M, Size.L, Size.XL);
-//        assertThat(StoreCategory.values()).contains(StoreCategory.FORMAL, StoreCategory.CASUAL,
-//                StoreCategory.SPORTSWEAR, StoreCategory.URBAN);
-//        assertThat(PrimaryColor.valueOf("ONYX_BLACK")).isEqualTo(PrimaryColor.ONYX_BLACK);
-//        assertThat(SecondaryColor.valueOf("SLATE")).isEqualTo(SecondaryColor.SLATE);
-//        assertThat(TertiaryColor.valueOf("RAW_GOLD")).isEqualTo(TertiaryColor.RAW_GOLD);
-//    }
-
     // ---- new version, coverage
     static Stream<Class<? extends  Enum<?>>> enumProvider(){
         return  Stream.of(
@@ -42,7 +32,6 @@ class DomainEnumCoverageTest {
                 ProductStatus.class,
                 VolumeType.class,
                 // store
-                StoreCategory.class,
                 StoreStatus.class,
                 PrimaryColor.class,
                 SecondaryColor.class,
